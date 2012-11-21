e90aaf88dbb08ddf66ce4fa00534b044c796c687
progand <lach@ukr.net>
2012-11-21 16:41:58 +0200

    [*] Register conroller created

---
+++ protected/views/register/supplier.php
@@ -0,0 +1,14 @@
+<?php
+/* @var $this RegisterController */
+
+$this->breadcrumbs=array(
+	'Register'=>array('/register'),
+	'Supplier',
+);
+?>
+<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
+
+<p>
+	You may change the content of this page by modifying
+	the file <tt><?php echo __FILE__; ?></tt>.
+</p>
