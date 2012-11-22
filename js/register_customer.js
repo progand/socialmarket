80e739bb8179ffc7e0eeb108aa7957613f980f3d
progand <lach@ukr.net>
2012-11-22 17:02:38 +0200

    [*] Register customer form ported

---
+++ js/register_customer.js
@@ -0,0 +1,160 @@
+var map = null;
+
+$(document).ready(function() {
+	formInit();
+});
+
+function formInit() {
+	$("#latitude,#longitude").change(locationChanged);
+	ymaps.ready(formLocation);
+}
+
+function locationChanged() {
+	var latitude = $("#latitude").val();
+	var longitude = $("#longitude").val();
+	map.setCenter([ latitude, longitude ]);
+}
+
+function formLocation() {
+	$("section").hide();
+	$("button#btn_location_correct").click(function() {
+		if (checkLocationForm()) {
+			formAccountInfo();
+		}
+	});
+	if (navigator.geolocation) {
+		$("section#allow_geolocation").fadeIn(1000);
+		navigator.geolocation.getCurrentPosition(gotPosition,
+				errorGettingPosition);
+	} else {
+		formLocationShow(ymaps.geolocation.latitude,
+				ymaps.geolocation.longitude);
+	}
+}
+
+function checkLocationForm() {
+	return true;
+}
+
+function formAccountInfo() {
+	$("section").hide();
+	$("section#account_info").fadeIn();
+	var address = getAddressAsString();
+	$("section#account_info p#address").text(address);
+}
+
+function getAddressAsString() {
+	var address = "" + $("#country").val();
+	if ($("#region").val().length > 0) {
+		address += ", " + $("#region").val();
+	}
+	if ($("#city").val().length > 0) {
+		address += ", " + $("#city").val();
+	}
+	if ($("#district").val().length > 0) {
+		address += ", " + $("#district").val();
+	}
+	if ($("#street").val().length > 0) {
+		address += ", " + $("#street").val();
+	}
+	if ($("#house").val().length > 0) {
+		address += ", " + $("#house").val();
+	}
+	if ($("#apartment").val().length > 0) {
+		address += ", " + $("#apartment").val();
+	}
+	return address;
+}
+
+function formLocationShow(latitude, longitude) {
+	if (!$("#latitude").val() && !$("#longitude").val()) {
+		$("section").hide();
+		$("section#location").fadeIn();
+	}
+
+	initMap(latitude, longitude);
+	$("#latitude").val(latitude);
+	$("#longitude").val(longitude);
+}
+
+function gotPosition(pos) {
+	var latitude = pos.coords.latitude;
+	var longitude = pos.coords.longitude;
+
+	formLocationShow(latitude, longitude);
+}
+
+function errorGettingPosition(err) {
+	formLocationShow(ymaps.geolocation.latitude, ymaps.geolocation.longitude);
+}
+
+function initMap(latitude, longitude) {
+	map = new ymaps.Map('map', {
+		center : [ latitude, longitude ],
+		zoom : 12
+	});
+	map.controls.add(new ymaps.control.ZoomControl());
+	map.controls.add('typeSelector');
+	map.controls.add('searchControl');
+	placemark = new ymaps.Placemark([ latitude, longitude ]);
+	map.geoObjects.add(placemark);
+	/*
+	 * map.geoObjects.add(new ymaps.Placemark([ ymaps.geolocation.latitude,
+	 * ymaps.geolocation.longitude ], { balloonContentHeader :
+	 * ymaps.geolocation.country, balloonContent : ymaps.geolocation.city,
+	 * balloonContentFooter : ymaps.geolocation.region }));
+	 */
+
+	map.geoObjects.events.add('click', function(e) {
+		var coords = e.get('target').geometry.getCoordinates();
+		var latitude = coords[0];
+		var longitude = coords[1];
+		$("#latitude").val(latitude);
+		$("#longitude").val(longitude);
+		updateAddress(latitude, longitude);
+	});
+
+	updateAddress(latitude, longitude);
+}
+
+function updateAddress(latitude, longitude) {
+	$("#latitude").val(latitude);
+	$("#longitude").val(longitude);
+	var myReverseGeocoder = ymaps.geocode([ latitude, longitude ]);
+	myReverseGeocoder.then(function(res) {
+		var street = res.geoObjects.get(-5);
+		if (street) {
+			$("#street").val(street.properties.get('name'));
+		} else {
+			$("#street").val("");
+		}
+
+		var district = res.geoObjects.get(-4);
+		if (district) {
+			$("#district").val(district.properties.get('name'));
+		} else {
+			$("#district").val("");
+		}
+
+		var city = res.geoObjects.get(-3);
+		if (city) {
+			$("#city").val(city.properties.get('name'));
+		} else {
+			$("#city").val("");
+		}
+
+		var region = res.geoObjects.get(-2);
+		if (region) {
+			$("#region").val(region.properties.get('name'));
+		} else {
+			$("#region").val("");
+		}
+
+		var country = res.geoObjects.get(-1);
+		if (country) {
+			$("#country").val(country.properties.get('name'));
+		} else {
+			$("#country").val("");
+		}
+	});
+}
--- protected/views/register/customer.php
+++ protected/views/register/customer.php
@@ -6,9 +6,50 @@ $this->breadcrumbs=array(
 	'Customer',
 );
 ?>
-<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
+<script src="js/jquery.js"></script>
+<script src="js/register_customer.js"></script>
+<script
+	src="http://api-maps.yandex.ru/2.0-stable/?load=package.full&lang=ru-RU"
+	type="text/javascript"></script>
+<link rel="stylesheet" href="css/register_customer.css"/>
 
-<p>
-	You may change the content of this page by modifying
-	the file <tt><?php echo __FILE__; ?></tt>.
-</p>
+<article>
+		<section id='loading'>
+			<img alt="" src="images/loading.gif">
+		</section>	
+		<section id='allow_geolocation'>
+			<h1>Please, allow geolocation in your browser (not necessarily)</h1>
+		</section>		
+		<section id="location">
+			<button id="btn_location_correct">Correct!</button>
+			<h1>Your location</h1>
+			<div id="map"></div>
+			<form action="#" id="location_form">
+				<h2>Coordinates</h2>
+				<span>Latitude:</span><input type="text" id="latitude"> <span>Longitude:</span><input
+					type="text" id="longitude"><br>
+				<h2>Address</h2>
+				<span>Country:</span><input type="text" required id="country">
+				<span>Region:</span><input type="text" id="region"><br>
+				<span>City:</span><input type="text" required id="city"> <span>District:</span><input
+					type="text" id="district"><br> <span>Street:</span><input
+					type="text" required id="street"> <span>House:</span><input
+					type="text" required id="house"><br> <span>Apartment:</span><input
+					type="text" id="apartment"><br>
+			</form>
+		</section>
+		<section id="account_info">
+			<h1>Last step</h1>
+			<form action="#">
+				<p id="address"></p>
+				<span>Login:</span><input type="text" required id="login"><br>
+				<span>E-mail:</span><input type="email" required id="email"><br>
+				<span>Full Name:</span><input type="text" required id="name"><br>
+				<span>Password:</span><input type="password" required id="password"><br>
+				<span>Re-type:</span><input type="password" required
+					id="password_confirm"><br>
+
+				<button id="btn_submit_register">Register now!</button>
+			</form>
+		</section>
+	</article>
