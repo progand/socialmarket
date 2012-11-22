a64a39e3d54e726530a4c2494e6bd246be750d3c
progand <lach@ukr.net>
2012-11-22 08:00:20 +0200

    [*] Design register/index

--- protected/views/register/index.php
+++ protected/views/register/index.php
@@ -2,12 +2,11 @@
 /* @var $this RegisterController */
 
 $this->breadcrumbs=array(
-	'Register',
+		'Register',
 );
 ?>
-<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
-
-<p>
-	You may change the content of this page by modifying
-	the file <tt><?php echo __FILE__; ?></tt>.
-</p>
+<h1>Choose</h1>
+<div class='register_user_type'>
+	<a href="/?r=register/customer" class='button'><span>I am customer</span></a> <a
+		href="/?r=register/supplier" class='button'><span>I am supplier</span></a>
+</div>
