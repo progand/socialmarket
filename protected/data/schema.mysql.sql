393a78d5e004fc922f80fb0e416708159476b201
progand <lach@ukr.net>
2012-11-23 15:49:00 +0200

    [*] db updated

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -1,3 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2
+-- http://www.phpmyadmin.net
+--
+-- Хост: localhost
+-- Время создания: Ноя 23 2012 г., 14:48
+-- Версия сервера: 5.5.25a
+-- Версия PHP: 5.4.4
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `socialmarket`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tbl_customer`
+--
+
 CREATE TABLE IF NOT EXISTS `tbl_customer` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
@@ -25,7 +53,8 @@ CREATE TABLE IF NOT EXISTS `tbl_user` (
   `username` varchar(128) NOT NULL,
   `password` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
 
 --
@@ -54,3 +83,7 @@ INSERT INTO `tbl_user` (`id`, `username`, `password`, `email`) VALUES
 (19, 'test19', 'pass19', 'test19@example.com'),
 (20, 'test20', 'pass20', 'test20@example.com'),
 (21, 'test21', 'pass21', 'test21@example.com');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
